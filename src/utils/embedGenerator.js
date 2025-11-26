f516735274bb0aee81ce1abee416705a422b7eab
hanbyul0103 <all007b@naver.com>
2025-11-26 11:35:38 +0900

    fix: embed generator

--- src/utils/embedGenerator.js
+++ src/utils/embedGenerator.js
@@ -1,9 +1,13 @@
 import { EmbedBuilder } from "discord.js";
 
 function createEmbed({ title, description, fields = [], timestamp = false }) {
-    const embed = new EmbedBuilder()
-        .setTitle(title)
-        .setDescription(description);
+    const embed = new EmbedBuilder();
+
+    if (title)
+        embed.setTitle(title);
+
+    if(description)
+        embed.setDescription(description);
 
     if (fields.length > 0) {
         embed.addFields(fields.map(f => ({
